2b70a0584d690d6a67e7bbe5f5d5aa2ab863de88
codewithingrid <65591295+codewithingrid@users.noreply.github.com>
2020-05-19 17:59:58 +1000

    Update and rename problem09.rtf to problem09.c

---
+++ problem09.c
@@ -0,0 +1,29 @@
+//A program written by <INSERT_NAME>
+//DATE: DD/MM/YEAR
+//Special Pythagorean triplet
+//A program which is calculating the pythagorean triplet such that a < b < c
+//and a*a + b*b = c*c
+
+#include <stdio.h>
+#define SUM 1000
+
+int main(void){
+    //Initialising the triplet
+    int a = 0;
+    int b = 0; 
+    int c = 0;
+    
+//NOTE EDIT THE CONDITIONS OF THE FOR LOOP!
+    for () {
+
+        for () {
+
+            //Calculating the c variable
+            
+
+            //Check for the pythagorean triplet 
+        
+        }
+    }
+}
+
